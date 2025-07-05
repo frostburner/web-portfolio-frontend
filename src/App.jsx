2af03238872c946b500227af1a044dbf8c4cd8b7
frostburner <villarafaeljoseph@gmail.com>
2025-07-05 14:49:22 +0800

    populate page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {Route, Routes} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Homepage from './Homepage/Homepage.jsx'
+import About from './About/About.jsx'
 import './App.css'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
       
       {/* Add more routes here as needed */}
     </Routes>
